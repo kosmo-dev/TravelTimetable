cd0eb30b54f277c9773dbc190c797a29644cac34
Vadim Kuzmin <kuzminvadima@yandex.ru>
2024-02-29 22:37:11 +0300

    Initial Commit

---
+++ TravelTimetable/TravelTimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTimetableApp.swift
+//  TravelTimetable
+//
+//  Created by Вадим Кузьмин on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelTimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
