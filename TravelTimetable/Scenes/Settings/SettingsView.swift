f86c5601e627515be364ca29d55835ea537fd291
Vadim Kuzmin <kuzminvadima@yandex.ru>
2024-05-02 18:25:32 +0300

    feat: add Settings view

--- TravelTimetable/Scenes/Settings/SettingsView.swift
+++ TravelTimetable/Scenes/Settings/SettingsView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var darkThemeIsOn = false
+    
     var body: some View {
-        Text("Settings")
+        mainView
+            .background(.ypWhiteDL)
+            
+    }
+    
+    var mainView: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text("Темная тема")
+                    .padding(.vertical, 19)
+                    .foregroundStyle(Color.ypBlackDL)
+                    .font(.system(size: 17))
+                Spacer()
+                Toggle("", isOn: $darkThemeIsOn)
+                    .tint(Color.ypBlue)
+            }
+            HStack {
+                Text("Пользовательское соглашение")
+                    .padding(.vertical, 19)
+                    .foregroundStyle(Color.ypBlackDL)
+                    .font(.system(size: 17))
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .fontWeight(.bold)
+            }
+            Spacer()
+            Text("Приложение использует API 'Яндекс.Расписания'")
+                .foregroundStyle(Color.ypBlackDL)
+                .font(.system(size: 12))
+                .padding(.bottom)
+            Text("Версия 1.0 (beta)")
+                .foregroundStyle(Color.ypBlackDL)
+                .font(.system(size: 12))
+        }
+        .padding(.top, 24)
+        .padding(.horizontal)
     }
 }
 
